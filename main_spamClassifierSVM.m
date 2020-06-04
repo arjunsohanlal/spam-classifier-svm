b00dd171ba26685f88fadaacbc5f9386c2466261
Arjun Sohanlal <arjunsohanlal@gmail.com>
2020-06-05 00:08:49 +0530

    Add files via upload

---
+++ main_spamClassifierSVM.m
@@ -0,0 +1,60 @@
+%% Initialization
+clear ; close all; clc
+
+% Load the Spam Email dataset
+% X, y loaded - X contains training examples in each row (4000 examples)
+% y contains classification labels for the same.
+load('spamTrain.mat');
+
+fprintf('\nTraining Linear SVM (Spam Classification)\n')
+
+C = 0.1;
+model = svmTrain(X, y, C, @linearKernel);
+
+p = svmPredict(model, X);
+
+fprintf('Training Accuracy: %f\n', mean(double(p == y)) * 100);
+
+% Load the test dataset
+% You will have Xtest, ytest in your environment
+load('spamTest.mat');
+
+fprintf('\nEvaluating the trained Linear SVM on a test set ...\n')
+
+p = svmPredict(model, Xtest);
+
+fprintf('Test Accuracy: %f\n', mean(double(p == ytest)) * 100);
+pause;
+
+
+%  Since the model we are training is a linear SVM, we can inspect the
+%  weights learned by the model to understand better how it is determining
+%  whether an email is spam or not. The following code finds the words with
+%  the highest weights in the classifier. Informally, the classifier
+%  'thinks' that these words are the most likely indicators of spam.
+%
+
+% Sort the weights and obtin the vocabulary list
+[weight, idx] = sort(model.w, 'descend');
+vocabList = getVocabList();
+
+fprintf('\nTop predictors of spam: \n');
+for i = 1:15
+    fprintf(' %-15s (%f) \n', vocabList{idx(i)}, weight(i));
+end
+
+fprintf('\n\n');
+fprintf('\nProgram paused. Press enter to continue.\n');
+pause;
+
+% Trying classifier on real emails
+filename = 'spamSample1.txt';
+
+% Read and predict
+file_contents = readFile(filename);
+word_indices  = processEmail(file_contents);
+x = emailFeatures(word_indices);
+p = svmPredict(model, x);
+
+fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
+fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
